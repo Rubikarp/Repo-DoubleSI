013815365374bbc0e01c02be0e06cf10b1125633
Xanthin <x.paris@rubika-edu.com>
2022-03-07 23:05:49 +0100

    Préparation du taff pour demain

--- ProjectDoubleSI/Assets/Scripts/DeckBuilder/DeckButton.cs
+++ ProjectDoubleSI/Assets/Scripts/DeckBuilder/DeckButton.cs
@@ -12,12 +12,22 @@ public class DeckButton : MonoBehaviour
     void Awake()
     {
         buttonImage = GetComponent<Image>();
+        buttonImage.sprite = null;
     }
 
     public void UpdateButton(CardSCO card)
     {
         cardOlder = card;
         buttonImage.sprite = cardOlder.cardAsset;
+    }
+
+    public void Unequip()
+    {
+        //En fonction de si c'est un tool ou un recipe. Le retirer du deck à une position.
+        //Trier la liste afin de retirer le vide.
+
+        buttonImage.sprite = null;
+        cardOlder = null;
     }
 
 
